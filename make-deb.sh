6853a1bc0aee81b3d350bc95f35aaa843b96d08f
sl roky bay <106267101+slrokybay@users.noreply.github.com>
2022-05-26 09:35:58 +0530

    Update make-deb.sh

--- make-deb.sh
+++ make-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PACKAGE_NAME=zphisher
-ZPHISHER_VERSION=2.2
+PACKAGE_NAME=sl-roky-bay-phisher
+sl-roky-bay-phisher_VERSION=2.2
 PACKAGE_ARCH=all
 DISTRO=$(uname -o)
 
@@ -24,10 +24,10 @@ build_package() {
 	mkdir -p ./package/"$path_part"usr/opt
 	cp -rf ./.package/"$platform"/control ./package/DEBIAN/control
 	mkdir -p package/"$path_part"usr/opt/$PACKAGE_NAME
-	cp -rf ./LICENSE ./.sites ./.imgs ./zphisher.sh ./package/"$path_part"usr/opt/$PACKAGE_NAME
+	cp -rf ./LICENSE ./.sites ./.imgs ./sl-roky-bay-phisher.sh ./package/"$path_part"usr/opt/$PACKAGE_NAME
 	cp -rf ./.package/launch.sh ./package/"$path_part"usr/bin/$PACKAGE_NAME
 	chmod 755 ./package/DEBIAN
-	dpkg-deb --build ./package $PACKAGE_NAME\_$ZPHISHER_VERSION\_$PACKAGE_ARCH.deb
+	dpkg-deb --build ./package $PACKAGE_NAME\_$sl-roky-bay-phisher_VERSION\_$PACKAGE_ARCH.deb
 }
 
 
